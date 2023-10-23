7deb91250ae841e9392d458895f4972f8bf13356
Doug Jones <websafe@cjboco.com>
2023-10-23 15:29:32 -0700

    fixed issue with last package update.

--- components/DrawingBoard.jsx
+++ components/DrawingBoard.jsx
@@ -1,47 +1,17 @@
-import React, { useRef, useEffect } from 'react';
-import { Primary, Secondary } from './Colors.jsx';
+import React, { useRef, useEffect, useState } from 'react';
+import { Secondary } from './Colors.jsx';
 import styled from 'styled-components';
 
-const px = (val) => {
+const px = ( val ) => {
   return val + 'px';
 };
 
-const drawOnCanvas = (canvasRef, hiddenImgRef, svgData) => {
-  if (
-    !canvasRef ||
-    !canvasRef.current ||
-    !hiddenImgRef ||
-    !hiddenImgRef.current ||
-    !svgData
-  ) {
-    return; // Should not happen, but do check anyway
-  }
-
-  const canvas = canvasRef.current;
-  const ctx = canvasRef.current.getContext('2d');
-  const myImg = hiddenImgRef.current;
-  myImg.src = svgData;
-
-  myImg.onload = (e) => {
-    const width = getComputedStyle(e.target)
-      .getPropertyValue('width')
-      .replace(/px$/, '');
-    const height = getComputedStyle(e.target)
-      .getPropertyValue('height')
-      .replace(/px$/, '');
-    canvas.width = width;
-    canvas.height = height;
-    ctx.clearRect(0, 0, width, width);
-    ctx.drawImage(myImg, 0, 0);
-  };
-};
-
 const Canvas = styled.canvas(
-  ({ width, height, data }) => `
+  ( { width, height, data } ) => `
     display: ${data?.svgData ? 'block' : 'none'}
-    width: 500px;
-    height: 500px;
-    border: 1px solid ${Secondary('light')};
+    width: ${px( width )};
+    height: ${px( height )};
+    border: 1px solid ${Secondary( 'light' )};
     transform: scale(1);
   `
 );
@@ -61,7 +31,7 @@ const Btn = styled.button`
   justify-content: center;
   background-color: white;
   border-radius: 0.4rem;
-  border: 1px solid ${Secondary('light')};
+  border: 1px solid ${Secondary( 'light' )};
   margin: 0.2rem;
   padding: 0.5rem 0.75rem;
   font-size: 1.25rem;
@@ -80,75 +50,95 @@ const Btn = styled.button`
 `;
 
 const HiddenImg = styled.img`
-  position: absolute;
-  top: -15999px;
-  left: -15999px;
-  width: auto;
-  height: auto;
+  width: auto !important;
+  height: auto !important;
+  object-fit: none;
 `;
 
-const DrawingBoard = ({ width, height, svgData }) => {
+const DrawingBoard = ( { width, height, svgData } ) => {
+  const [ isLoaded, setIsLoaded ] = useState( false );
+
   const myCanvas = useRef();
   const hiddenImg = useRef();
   const btnGIF = useRef();
   const btnPNG = useRef();
 
-  useEffect(() => {
-    drawOnCanvas(myCanvas, hiddenImg, svgData);
-  }, [svgData, myCanvas, hiddenImg]);
+  useEffect( () => {
+    if ( isLoaded && myCanvas?.current && hiddenImg?.current ) {
+      const ctx = myCanvas.current.getContext( '2d' );
+      const width = hiddenImg.current.clientWidth;
+      const height = hiddenImg.current.clientHeight;
+      myCanvas.current.width = width;
+      myCanvas.current.height = height;
+      ctx.clearRect( 0, 0, width, width );
+      ctx.drawImage( hiddenImg.current, 0, 0 );
+    }
+  }, [ isLoaded, myCanvas, hiddenImg ] );
 
-  const handleClick = (e) => {
+  const handleClick = ( e ) => {
     e.preventDefault();
 
-    const type = e.currentTarget.getAttribute('data-type');
+    const type = e.currentTarget.getAttribute( 'data-type' );
     const canvas = myCanvas.current;
-    const anchor = document.createElement('a');
+    const anchor = document.createElement( 'a' );
 
-    if (type === 'GIF') {
+    if ( type === 'GIF' ) {
       anchor.download = 'converted-svg.gif';
-      anchor.href = canvas.toDataURL('image/gif');
-    } else if (type === 'PNG') {
+      anchor.href = canvas.toDataURL( 'image/gif' );
+    } else if ( type === 'PNG' ) {
       anchor.download = 'converted-svg.png';
-      anchor.href = canvas.toDataURL('image/png');
+      anchor.href = canvas.toDataURL( 'image/png' );
     }
 
     anchor.click();
     anchor.remove();
   };
 
+  if ( !svgData ) {
+    return null;
+  }
+
   return (
     <>
-      <HiddenImg ref={hiddenImg} />
-      {svgData ? (
-        <>
-          <Canvas
-            width={width}
-            height={height}
-            data={svgData}
-            ref={myCanvas}
-          />
-          <BtnGroup>
-            <Btn
-              role='button'
-              onClick={handleClick}
-              data-type='GIF'
-              ref={btnGIF}
-            >
+      <div style={{
+        position: 'absolute',
+        top: '90vw',
+        left: '90vh',
+        width: '1440px',
+        height: '1440px',
+        zIindex: '10',
+        overflow: 'hidden'
+      }}>
+        <HiddenImg
+          src={svgData}
+          onLoad={() => setIsLoaded( true )}
+          ref={hiddenImg}
+        />
+      </div>
+      <Canvas
+        width={width}
+        height={height}
+        data={svgData}
+        ref={myCanvas}
+      />
+      <BtnGroup>
+        <Btn
+          role='button'
+          onClick={handleClick}
+          data-type='GIF'
+          ref={btnGIF}
+        >
               Download - GIF
-            </Btn>
-            <Btn
-              role='button'
-              onClick={handleClick}
-              data-type='PNG'
-              ref={btnPNG}
-            >
+        </Btn>
+        <Btn
+          role='button'
+          onClick={handleClick}
+          data-type='PNG'
+          ref={btnPNG}
+        >
               Download - PNG
-            </Btn>
-          </BtnGroup>
-        </>
-      ) : (
-        ''
-      )}
+        </Btn>
+      </BtnGroup>
     </>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -9,121 +9,127 @@ import Loading from '../components/Loading';
 import Btn from '../components/Button';
 
 export default function Home() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [dropZoneHidden, setDropZoneHidden] = useState(false);
-  const [canvasData, setCanvasData] = useState(null);
+  const [ isLoading, setIsLoading ] = useState( false );
+  const [ dropZoneHidden, setDropZoneHidden ] = useState( false );
+  const [ canvasData, setCanvasData ] = useState( null );
 
   // onDrop function
-  const onDrop = useCallback((acceptedFiles) => {
+  const onDrop = useCallback( ( acceptedFiles ) => {
     // this callback will be called after files get dropped, we will get the acceptedFiles. If you want, you can even access the rejected files too
     // Loop through accepted files
-    setIsLoading(true);
-    acceptedFiles.map((file) => {
+    setIsLoading( true );
+    acceptedFiles.map( ( file ) => {
       // Initialize FileReader browser API
       const reader = new FileReader();
       // onload callback gets called after the reader reads the file data
-      reader.onload = (e) => {
+      reader.onload = ( e ) => {
         // setImages((prevState) => [...prevState, { src: e.target.result }]);
         // add the image into the state. Since FileReader reading process is asynchronous, its better to get the latest snapshot state (i.e., prevState) and update it.
-        setCanvasData(e.target.result);
-        setDropZoneHidden(true);
-        setIsLoading(false);
+        setCanvasData( e.target.result );
+        setDropZoneHidden( true );
+        setIsLoading( false );
       };
       // Read the file as Data URL (since we accept only images)
-      reader.readAsDataURL(file);
+      reader.readAsDataURL( file );
       return file;
-    });
-  }, []);
+    } );
+  }, [] );
 
-  const handleReset = (e) => {
+  const handleReset = ( e ) => {
     e.preventDefault();
 
-    setCanvasData(null);
-    setIsLoading(false);
-    setDropZoneHidden(false);
+    setCanvasData( null );
+    setIsLoading( false );
+    setDropZoneHidden( false );
   };
 
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>SVG-Whiz</title>
         <meta name='description' content='Convert SVG images with ease!' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <div className='status-badges'>
-        <a
-          href='https://github.com/cjboco/svg-whiz'
-          aria-label='Link to the Github repository.'
-        >
-          <img
-            alt='GitHub forks'
-            src='https://img.shields.io/github/forks/cjboco/svg-whiz?style=social'
-            aria-label='Github forks display badge'
-          />
-        </a>
-        <a
-          href='https://github.com/cjboco/svg-whiz'
-          aria-label='Link to the Github repository.'
-        >
-          <img
-            alt='GitHub Repo stars'
-            src='https://img.shields.io/github/stars/cjboco/svg-whiz?style=social'
-            aria-label='Github repo stars display badge'
-          />
-        </a>
-      </div>
-
-      {/* http://codepo8.github.io/css-fork-on-github-ribbon/ */}
-      <span id='forkongithub'>
-        <a
-          href='https://github.com/cjboco/svg-whiz'
-          aria-label='Click to fork this project on Github.com'
-        >
-          Fork me on GitHub
-        </a>
-      </span>
-
-      <main className={styles.main}>
-        <Image
-          src={
-            '/tiny-people-drawing-with-pen-graphic-editor-isolated-flat-design.jpg'
-          }
-          width={720}
-          height={500}
-          priority={true}
-          alt={
-            'Splash graphic of tiny people working on a large computer monitor.'
-          }
-        />
-        <h1 className={styles.title}>Welcome to SVG-Whiz.</h1>
-        <Dropzone
-          onDrop={onDrop}
-          accept={{'image/svg+xml': []}}
-          isHidden={dropZoneHidden}
-        />
-        <DrawingBoard width={500} height={500} svgData={canvasData} />
-        <Loading visible={isLoading} />
-        <Btn
-          handleClick={handleReset}
-          isPrimary={true}
-          isHidden={!dropZoneHidden ? true : false}
-          label='Reset'
-        />
-      </main>
-
-      <footer className={styles.footer}>
-        <div>Copyright &copy; 2023 Doug Jones.</div>
-        <div>
-          Built by{' '}
+      <div className={styles.container}>
+        <div className='status-badges'>
           <a
-            href='https://cjboco.com'
-            aria-label='Link to Creative Juices, Bo. Co. website'
+            href='https://github.com/cjboco/svg-whiz'
+            aria-label='Link to the Github repository.'
           >
-            Creative Juices, Bo. Co.
+            <img
+              alt='GitHub forks'
+              src='https://img.shields.io/github/forks/cjboco/svg-whiz?style=social'
+              aria-label='Github forks display badge'
+            />
+          </a>
+          <a
+            href='https://github.com/cjboco/svg-whiz'
+            aria-label='Link to the Github repository.'
+          >
+            <img
+              alt='GitHub Repo stars'
+              src='https://img.shields.io/github/stars/cjboco/svg-whiz?style=social'
+              aria-label='Github repo stars display badge'
+            />
           </a>
         </div>
-      </footer>
-    </div>
+
+        {/* http://codepo8.github.io/css-fork-on-github-ribbon/ */}
+        <span id='forkongithub'>
+          <a
+            href='https://github.com/cjboco/svg-whiz'
+            aria-label='Click to fork this project on Github.com'
+          >
+          Fork me on GitHub
+          </a>
+        </span>
+
+        <main className={styles.main}>
+          <Image
+            src={
+              '/tiny-people-drawing-with-pen-graphic-editor-isolated-flat-design.jpg'
+            }
+            width={720}
+            height={500}
+            priority={true}
+            alt={
+              'Splash graphic of tiny people working on a large computer monitor.'
+            }
+          />
+          <h1 className={styles.title}>Welcome to SVG-Whiz.</h1>
+          <Dropzone
+            onDrop={onDrop}
+            accept={{ 'image/svg+xml': [] }}
+            isHidden={dropZoneHidden}
+          />
+          <DrawingBoard
+            width={500}
+            height={500}
+            svgData={canvasData}
+          />
+          <Loading visible={isLoading} />
+          <Btn
+            handleClick={handleReset}
+            isPrimary={true}
+            isHidden={!dropZoneHidden}
+            label={'Reset'}
+          />
+        </main>
+
+        <footer className={styles.footer}>
+          <div>Copyright &copy; 2023 Doug Jones.</div>
+          <div>
+          Built by{' '}
+            <a
+              href='https://cjboco.com'
+              aria-label='Link to Creative Juices, Bo. Co. website'
+            >
+            Creative Juices, Bo. Co.
+            </a>
+          </div>
+        </footer>
+      </div>
+    </>
   );
 }
